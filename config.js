e26d0f5b69ef444057d0f621ab0092368230494a
idsherma <idsherma@gmail.com>
2020-08-28 22:10:24 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '615153382693623',
+        clientSecret: '4c8dff4323b168afd98ec54bdbc190f7'
+    }
 }
--- models/user.js
+++ models/user.js
@@ -8,9 +8,10 @@ const userSchema = new Schema({
         default: ''
     },
     lastname: {
-    type: String,
+        type: String,
         default: ''
     },
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
